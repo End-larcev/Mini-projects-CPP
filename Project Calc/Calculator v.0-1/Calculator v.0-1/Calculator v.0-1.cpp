41570a99a8d94176d49cf198b7a61f656e8a01ac
End-larcev <eduard.end.larcev@outlook.com>
2023-11-18 13:40:44 +0300

    Calculator v.0-1
    Спрашивает число, операцию, число. Даёт ответ.

---
+++ Project Calc/Calculator v.0-1/Calculator v.0-1/Calculator v.0-1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n1, n2;
+	char oper;
+
+	cout << "Hi, it's my calculator v.0-1" << endl;
+	cout << "Enter your first number: "; cin >> n1;
+	cout << "Ok, and choose, what you want to do?\n"
+		<< "+, -, * or /   "; cin >> oper;
+	cout << "And, enter second number: "; cin >> n2;
+	cout << endl;
+	cout << "---------------------------------------" << endl;
+
+	switch (oper)
+	{
+	case '+':
+		cout << "Answer: " << n1 + n2 << endl;
+		break;
+	case '-':
+		cout << "Answer: " << n1 - n2 << endl;
+		break;
+	case '*':
+		cout << "Answer: " << n1 * n2 << endl;
+		break;
+	case '/':
+		cout << "Answer: " << n1 / n2 << endl;
+		break;
+	}
+}
