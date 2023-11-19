14f8ba0e26a52675ae1fa429b15b74bc3be588fd
End-larcev <eduard.end.larcev@outlook.com>
2023-11-19 16:35:44 +0300

    Bug fix v.0-2-2
    добавил в функцию "поиска оператора" поиск ответа, так как синтаксис не позволяет оперировать числа со значением переменной(чаровым). а делать операции в зависимости от оператора в главной функции слишком не разумно, поэтому я запихнул вычисление ответа в функцию поиска оператора. как по мне получилось оптимизированно :D

--- Project Calc/Calculator v.0-2-2/Calculator v.0-2-2/Calculator v.0-2-2.cpp
+++ Project Calc/Calculator v.0-2-2/Calculator v.0-2-2/Calculator v.0-2-2.cpp
@@ -1,11 +1,11 @@
 ﻿#include <iostream>
 using namespace std;
 
-void operator_acq(int& a, int& b, char& oper);
+void operator_acq_answ(int& a, int& b, char& oper, int&ans);
 
 int main()
 {
-    int n1, n2;
+    int n1, n2, ans;
     char oper;
 
     system("color 1F");
@@ -16,18 +16,18 @@ int main()
     system("cls");
 
     cout << "Ok, and choose, what you want to do?" << endl;
-    operator_acq(n1, n2, oper);
+    operator_acq_answ(n1, n2, oper, ans);
     cout << "Your operator: " << oper << endl;
     cout << "Your numbers: " << n1 << " and " << n2 << endl;
     cout << "-------------------------------" << endl;
-    cout << "Answer: " << n1 + n2 << "\n" << endl;
+    cout << "Answer: " << ans << "\n" << endl;
     system("pause");
 
     system("cls");
     cout << "Thanks for using the Calculator v.0-2-2" << endl;
 }
 
-void operator_acq(int& a, int& b, char& oper)
+void operator_acq_answ(int& a, int& b, char& oper, int&ans)
 {
     cout << "Enter the operation symbol: +, -, *, /" << endl;
     cout << "---------------------------" << endl;
@@ -38,18 +38,22 @@ void operator_acq(int& a, int& b, char& oper)
     {
     case '+':
         oper = '+';
+        ans = a + b;
         break;
     case '-':
         oper = '-';
+        ans = a - b;
         break;
     case '*':
         oper = '*';
+        ans = a * b;
         break;
     case '/':
         oper = '/';
+        ans = a / b;
         break;
     default:
         system("cls");
-        return(operator_acq(a, b, oper));
+        return(operator_acq_answ(a, b, oper, ans));
     }
 }
