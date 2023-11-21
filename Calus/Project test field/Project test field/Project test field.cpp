ccf66ae08e8a7553c9597b577f23e40fe57be034
End-larcev <eduard.end.larcev@outlook.com>
2023-11-21 20:32:54 +0300

    Creating new project: CryCo.      Rename project: Calus
    Создание нового проекта Cryco и переименование Calculator -> Calus.

---
+++ Calus/Project test field/Project test field/Project test field.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+void operator_acq_answ(int& pn1, int& pn2, char& oper, int& answ);
+
+int main()
+{
+    int n1, n2, ans;
+    char oper;
+
+    system("color 1F");
+    cout << "Hi, it's Calculator v.---" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Enter first number: "; cin >> n1;
+    cout << "Enter second number: "; cin >> n2;
+    system("cls");
+
+    cout << "Ok, and choose, what you want to do?" << endl;
+    operator_acq_answ(n1, n2, oper, ans);
+    cout << "Your operator: " << oper << endl;
+    cout << "Your numbers: " << n1 << " and " << n2 << endl;
+    cout << "-------------------------------" << endl;
+    cout << "Answer: " << ans << "\n" << endl;
+    system("pause");
+
+    system("cls");
+    cout << "Thanks for using the Calculator v.---" << endl;
+}
+
+void operator_acq_answ(int& pn1, int& pn2, char& oper, int& answ)
+{
+    cout << "Enter the operation symbol: +, -, *, /" << endl;
+    cout << "---------------------------" << endl;
+    cin >> oper;
+    system("cls");
+
+    switch (oper)
+    {
+    case '+':
+        oper = '+';
+        answ = pn1 + pn2;
+        break;
+    case '-':
+        oper = '-';
+        answ = pn1 - pn2;
+        break;
+    case '*':
+        oper = '*';
+        answ = pn1 * pn2;
+        break;
+    case '/':
+        oper = '/';
+        answ = pn1 / pn2;
+        break;
+    default:
+        system("cls");
+        return(operator_acq_answ(pn1, pn2, oper, answ));
+    }
+}
---
+++ CryCo/Project test field/Project test field/Project test field.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hi, this programm count crypto-$" << endl;
+    double budget, n1, n2, buy, sell, answ1, answ2, comm1, comm2;
+
+    cout << "Enter your capital: "; cin >> budget;
+    cout << "-----------------------" << endl;
+    cout << "Enter first price: "; cin >> n1;
+    cout << "Enter second price: "; cin >> n2;
+    system("cls");
+
+    // покупка крипты
+    buy = budget / n1; // то, сколько монет ты купил, без учета комиссии
+    comm1 = buy * 0.002; // коммиссия с твоей покупки 0,2%
+    answ1 = buy - comm1; // то, сколько реально ты купил
+
+    // продажа крипты
+    sell = answ1 * n2; // то, сколько ты продал, без учета комиссии
+    comm2 = sell * 0.002; // комиссия с твоей продажи 0,2%
+    answ2 = sell - comm2; // то, сколько ты реально продал
+
+    cout << "Start budget: " << budget << "$" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Crypto comission: " << comm1 << endl;
+    cout << "Dollar comission: " << comm2 << endl;
+    cout << "-----------------------" << endl;
+    cout << "Finish budget: " << answ2 << "$" << endl;
+
+    system("pause");
+    system("cls");
+    return(main());
+}
